53a4dd175a4d670886e5de8cdeb6db499a6ba5cc
Priyesh Patel <ppatel@broadleafcommerce.org>
2012-12-20 01:00:16 -0600

    add copy right headers

--- src/main/java/com/broadleafcommerce/admin/client/i18n/presenter/ProductOptionModiferView.java
+++ src/main/java/com/broadleafcommerce/admin/client/i18n/presenter/ProductOptionModiferView.java
@@ -15,7 +15,23 @@
  */
 
 package com.broadleafcommerce.admin.client.i18n.presenter;
-
+/* 
+ * Broadleaf Commerce Confidential
+ * _______________________________
+ * 
+ *  [2009] - [2013] Broadleaf Commerce, LLC 
+ *  All Rights Reserved.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Broadleaf Commerce, LLC
+ * The intellectual and technical concepts contained
+ * herein are proprietary to Broadleaf Commerce, LLC
+ * and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Broadleaf Commerce, LLC.
+ */
 import org.broadleafcommerce.openadmin.client.BLCMain;
 import org.broadleafcommerce.openadmin.client.view.ViewModifierAdapter;
 import org.broadleafcommerce.openadmin.client.view.dynamic.SubItemView;
